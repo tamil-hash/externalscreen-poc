b07c7b2ab406712313dda9dab9552989ab905e3c
Tamil Selvan S <tamil.selvans@raweng.com>
2024-02-20 13:06:32 +0530

    main

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+import ExternalScreen from './ExternalScreen/ExternalScreen';
+
+function App() {
+  return (
+    <ExternalScreen />
+  );
+}
+
+export default App;
---
+++ src/ExternalScreen/ExternalScreen.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ExternalScreen = () => {
+  return (
+    <div>ExternalScreen</div>
+  )
+}
+
+export default ExternalScreen;
